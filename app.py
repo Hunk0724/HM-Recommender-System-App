600379d501c9f7169e7a5819ccb682d917f8aad5
Hunk0724 <hunk123321123@gmail.com>
2024-06-13 13:00:08 +0800

    try

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import pickle
 # import surprise
-import sklearn
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
